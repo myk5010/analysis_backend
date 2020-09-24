dc2ec07f517c8009ec6a4320c714608911580a23
WONDFO\yongke.mai <273775054@qq.com>
2020-09-24 15:32:12 +0800

    修改配置/扩展

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,47 +1,58 @@
 import os
 from flask import Flask
-from flaskr.extensions import db
+import click
+# 配置
+from flaskr.settings import config
+# 扩展
+from flaskr.extensions import db, migrate
 # 蓝图
 from flaskr.blueprints.admin import admin_bp
+# 数据库模型
+from flaskr.models import user, materiel, stock
 
-def create_app(test_config = None):
-  # create and configure the app
-  app = Flask(__name__, instance_relative_config = True)
-  app.config.from_mapping(
-    SECRET_KEY = 'dev',
-    # DATABASE = os.path.join(app.instance_path, 'flask.sqlite'),
-    SQLALCHEMY_DATABASE_URI = os.path.join(app.instance_path, 'flask.sqlite'),
-    SQLALCHEMY_TRACK_MODIFICATIONS = False,
-  )
+def create_app(config_name = None):
+  if config_name is None:
+    config_name = os.getenv('FLASK_CONFIG', 'development')
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
 
-  if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent = True)
-  else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
-
-  # ensure the instance folder exists
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
-
-  # a simple page that says hello
-  @app.route('/hello')
-  def hello():
-    return 'Hello, World!'
-
+  # 注册
   register_extensions(app)
+  register_shell_context(app)
   register_blueprints(app)
+  register_commands(app)
 
   return app
 
+
+# 初始化扩展
 def register_extensions(app):
-  # 初始化扩展
   db.init_app(app)
+  migrate.init_app(app, db)
 
 
+# 蓝图
 def register_blueprints(app):
-  # 注册蓝图
   app.register_blueprint(admin_bp)
+
+
+def register_shell_context(app):
+  @app.shell_context_processor
+  def make_shell_context():
+    # 数据库模型
+    return dict(db=db, user=user, materiel=materiel, stock=stock)
+
+
+# 指令
+def register_commands(app):
+  @app.cli.command()
+  @click.option('--drop', is_flag=True, help='Create after drop.')
+  # 初始化数据库
+  def initdb(drop):
+    """Initialize the database."""
+    if drop:
+      click.confirm('This operation will delete the database, do you want to continue?', abort=True)
+      db.drop_all()
+      click.echo('Drop tables.')
+    db.create_all()
+    click.echo('Initialized database.')
--- flaskr/extensions.py
+++ flaskr/extensions.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
-# 数据库管理
+# 数据库管理扩展
 db = SQLAlchemy()
+# 数据迁移
+migrate = Migrate()
